8f5a630cdbf8b3672baccfb1ee1f230847c745e4
coo_coo1 <kaloianbch@gmail.com>
2018-04-07 05:07:27 +0100

    Update RecyclerViewAdapter

--- app/src/main/java/xyz/chokanov/kalchat/RecyclerViewAdapter.java
+++ app/src/main/java/xyz/chokanov/kalchat/RecyclerViewAdapter.java
@@ -1,44 +1,54 @@
 package xyz.chokanov.kalchat;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+
+/**
+ * Created by User on 1/1/2018.
+ */
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapter";
+
     private ArrayList<String> mMessages = new ArrayList<>();
-    private ArrayList<String> mUsers = new ArrayList<>();
+    private ArrayList<String> mUserNames = new ArrayList<>();
     private Context mContext;
 
-    public RecyclerViewAdapter(ArrayList<String> mMessages, ArrayList<String> mUsers, Context mContext) {
-        this.mMessages = mMessages;
-        this.mUsers = mUsers;
-        this.mContext = mContext;
+    public RecyclerViewAdapter(Context context, ArrayList<String> messages, ArrayList<String> userNames ) {
+        mMessages = messages;
+        mUserNames = userNames;
+        mContext = context;
     }
 
-    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
         Log.d(TAG, "onBindViewHolder: called.");
-        holder.mUser.setText(mUsers.get(position));
+
         holder.mMessage.setText(mMessages.get(position));
+        holder.mUserName.setText(mUserNames.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
     }
 
     @Override
@@ -46,18 +56,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return mMessages.size();
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        ImageView mAvatar;
-        TextView mMessage, mUser;
-        RelativeLayout mParentLayout;
+        TextView mUserName;
+        TextView mMessage;
+        RelativeLayout parentLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            mAvatar = itemView.findViewById(R.id.imgAvatar);
-            mUser = itemView.findViewById(R.id.txtUser);
-            mParentLayout = itemView.findViewById(R.id.layoutItem);
+            mUserName = itemView.findViewById(R.id.txtUser);
+            mMessage = itemView.findViewById(R.id.txtMsg);
+            parentLayout = itemView.findViewById(R.id.layoutItem);
         }
     }
 }
+
+
+
 
