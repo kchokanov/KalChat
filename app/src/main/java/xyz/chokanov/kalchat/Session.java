5d47fde452bd9183232807e6b5a3728f64a7c703
coo_coo1 <kaloianbch@gmail.com>
2018-04-07 19:02:10 +0100

    Add Mac Adddress getter and MD5 hash function to Session

--- app/src/main/java/xyz/chokanov/kalchat/Session.java
+++ app/src/main/java/xyz/chokanov/kalchat/Session.java
@@ -1,5 +1,11 @@
 package xyz.chokanov.kalchat;
 
+import java.math.BigInteger;
+import java.net.NetworkInterface;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Session {
@@ -7,9 +13,46 @@ public class Session {
 
     public Session(){
         username = "Kal#" + new Random().nextInt(1337);
+
     }
 
     public static String getUsername() {
         return username;
+    }
+    public String getHash (String input){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(input.getBytes(), 0, input.length());
+            return(new BigInteger(1, md.digest())).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    public static String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(Integer.toString(b & 0xFF));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+            //handle exception
+        }
+        return "";
     }
 }
